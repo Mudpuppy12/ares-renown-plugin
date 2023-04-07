3356a61acfb8fb0f00cf20caf129e1da2c160e7b
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2023-04-07 20:57:47 +0200

    Update custom-routes.js

--- custom/custom-routes.js
+++ custom/custom-routes.js
@@ -2,10 +2,6 @@ export default function setupCustomRoutes(router) {
   // Define your own custom routes here, just as you would in router.js but using 'router' instead of 'this'.
   // For example:
   // router.route('yourroute');
- router.route('courtesan-management');
- router.route('courtesan-management-house', { path: '/courtesan-management/:house' });
- router.route('econ-management');
- router.route('factors');
  router.route('renown-management');
  router.route('renown-management-org', { path: '/renown-management/:org' });
  router.route('renown-top');
