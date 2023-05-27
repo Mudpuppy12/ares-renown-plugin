db8377a484e1d74648811dd50a9cb67b3bee2805
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2023-05-27 10:55:33 +0200

    Update cron_event_handler.rb

--- plugin/events/cron_event_handler.rb
+++ plugin/events/cron_event_handler.rb
@@ -9,14 +9,16 @@ module AresMUSH
           days = Global.read_config("renown", "cron_renown_days")
           message_title = Global.read_config("renown", "cron_message_title")
           name = Renown.get_max_renown_char(days)
-          points = Renown.prettify(Renown.calculate_gained_time(name,days))
-          title = message_title + ": " + name
-          Forum.system_post(
-            Global.read_config("renown", "cron_message_category"),
-            title, 
-            t('renown.regular_renown_message', :name => name, :points => points, :days => days, :renown_name => Renown.title.downcase ))
-          target = Character.find_one_by_name(name)
-          Achievements.award_achievement(target, "renown_leading")
+          if (name != "Nobody")
+            points = Renown.prettify(Renown.calculate_gained_time(name,days))
+            title = message_title + ": " + name
+            Forum.system_post(
+              Global.read_config("renown", "cron_message_category"),
+              title, 
+              t('renown.regular_renown_message', :name => name, :points => points, :days => days, :renown_name => Renown.title.downcase ))
+            target = Character.find_one_by_name(name)
+            Achievements.award_achievement(target, "renown_leading")
+          end
         end
       end 
 
