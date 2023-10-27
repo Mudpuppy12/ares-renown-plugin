956415cc3e00b7c1208cdac9e23ffd6a50f609e0
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2023-10-27 17:05:41 +0200

    char and org links for renown-top

--- plugin/web/renown_top_request_handler.rb
+++ plugin/web/renown_top_request_handler.rb
@@ -7,12 +7,17 @@ module AresMUSH
         toporgs = Renown.get_top_list(Renown.group,10)
         title = Renown.title
         org_header = Renown.plural(Renown.group).titleize
+        show_org_links = Global.read_config("renown","org_links")
+        org_page_prefix = Global.read_config("renown","org_page_prefix")
+        org_page_prefix = (org_page_prefix == {}) ? "" : org_page_prefix
 
         {
           chars: topchars,
           orgs: toporgs,
           renown_title: title,
-          orgname: org_header
+          orgname: org_header,
+          show_links: show_org_links,
+          org_prefix: org_page_prefix
         }
       end
 
